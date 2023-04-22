8c248c8231d3a84326bdc403050cb6ce1448e3e2
kei_creativelife <68884990+kei-creative-life@users.noreply.github.com>
2023-04-23 01:41:47 +0900

    Make Vertical Time Line Component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { VerticalTimeLine } from './components'
+
+const sections = [
+  { title: 'タイトル1', content: 'ここに文章を入れます。ここに文章を入れます。ここに文章を入れます' },
+  { title: 'タイトル2', content: 'ここに文章を入れます。ここに文章を入れます。ここに文章を入れます' },
+]
+</script>
 
 <template>
   <!-- <WindowBox /> -->
-  <TerminalBox />
+  <VerticalTimeLine :sections="sections" />
   <!-- <ProgressRectangle />
   <ProgressCircular width="48" height="48" color="blue" /> -->
+  <TerminalBox />
 </template>
 
 <style scoped></style>
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as ProgressRectangle } from './ProgressRectangle/ProgressRectan
 export { default as ProgressCircular } from './ProgrssCircular/ProgressCircular.vue'
 export { default as WindowBox } from './WindowBox/WindowBox.vue'
 export { default as TerminalBox } from './TerminalBox/TerminalBox.vue'
+export { default as VerticalTimeLine } from './VerticalTimeLine/VerticalTimeLine.vue'
