1e98884ddb257247e4a71044c7da2ca9c644499f
kei_creativelife <68884990+kei-creative-life@users.noreply.github.com>
2023-04-22 19:53:58 +0900

    update TerminalBox css

--- src/components/TerminalBox/TerminalBox.vue
+++ src/components/TerminalBox/TerminalBox.vue
@@ -1,58 +1,62 @@
 <script setup lang="ts">
-defineProps({
-  text: { type: String, default: 'This is text.This is text.This is text.This is text.This is text.This is text.This is text.This is text.' },
-  textColor: { type: String, default: '#000000' },
-  headerColor: { type: String, default: '#000000' },
-  borderColor: { type: String, default: '#000000' },
+import { computed, defineProps } from 'vue'
+
+const props = defineProps({
+  text: { type: String, default: '' },
+  textColor: { type: String, default: '#ffffff' },
+  textWeight: { type: String, default: 'bold' },
+  textSize: { type: String, default: '18px' },
+  headerColor: { type: String, default: 'rgba(0, 0, 0, 0.6)' },
   borderRadius: { type: String, default: '5px' },
-  bgColor: { type: String, default: '#000000' },
+  bgColor: { type: String, default: 'rgba(0, 0, 0, 0.8)' },
   leftColor: { type: String, default: '#f48384' },
   middleColor: { type: String, default: '#fbd172' },
   rightColor: { type: String, default: '#82c880' },
   maxWidth: { type: String, default: '500px' },
+  icons: { type: Boolean, default: false },
 })
+
+const styles = computed(() => ({
+  '--text-color': props.textColor,
+  '--text-weight': props.textWeight,
+  '--text-size': props.textSize,
+  '--bg-color': props.bgColor,
+  '--max-width': props.maxWidth,
+  '--header-color': props.headerColor,
+}))
 </script>
 
 <template>
-  <div class="terminal-box">
+  <div class="terminal-box" :style="styles">
     <div class="terminal-box_header">
-      <svg xmlns="http://www.w3.org/2000/svg" role="img" aria-label="ウインドウのボタン">
+      <svg xmlns="http://www.w3.org/2000/svg" role="img" aria-label="window-button">
         <circle cx="15" cy="15" r="5.5" :fill="leftColor" />
         <circle cx="35" cy="15" r="5.5" :fill="middleColor" />
         <circle cx="55" cy="15" r="5.5" :fill="rightColor" />
       </svg>
     </div>
     <div class="terminal-box_content">
-      <div class="heading-017">
-        <span>🍎</span>
-        <span>🏠~</span>
+      <div v-if="!icons" class="terminal-box_icon-group">
+        <div class="terminal-box_icon-wrapper">
+          <mdicon class="terminal-box_icon-first" name="apple" :width="20" :height="20" />
+          <div class="terminal-box_icon-second-wrapper">
+            <mdicon class="terminal-box_icon-second" name="folder-open" :width="20" :height="20" />
+            <!-- <span class="terminal-box_icon-path">~/Users/kengo.yamamoto/project_self/codebox-blog</span> -->
+          </div>
+        </div>
       </div>
-      <div class="terminal-box_angle">
-        <div class="terminal-box_angle-body" />
-      </div>
-      <p>{{ text }}</p>
+      <p class="terminal-box_content-text">
+        {{ text }}
+      </p>
     </div>
   </div>
 </template>
 
 <style scoped>
-.terminal-box_angle {
-  position: relative;
-}
-.terminal-box_angle-body {
-  position: absolute;
-  right: 0%;
-  width: 15px;
-  height: 30px;
-  background: linear-gradient(to bottom left, transparent 50%, blue 50%) top left/ 100% 50% no-repeat,
-    linear-gradient(to top left, transparent 50%, blue 50%) bottom left / 100% 50% no-repeat;
-}
-
 .terminal-box {
   position: relative;
-  max-width: 800px;
+  max-width: var(--max-width);
   margin: 0 auto;
-  border: 2px solid rgba(0, 0, 0, 0.7);
   border-radius: 5px;
   overflow: auto;
 }
@@ -63,7 +67,7 @@ defineProps({
   left: 0;
   height: 30px;
   width: 100%;
-  background-color: rgba(0, 0, 0, 0.7);
+  background-color: var(--header-color);
 }
 
 .terminal-box_header {
@@ -72,48 +76,74 @@ defineProps({
 
 .terminal-box_content {
   display: flex;
-  padding: 20px 30px 20px 10px;
-  background-color: rgba(0, 0, 0, 0.8);
-}
-
-.heading-017 {
-  display: flex;
-  justify-content: start;
+  flex-wrap: wrap;
   align-items: center;
-  position: relative;
-  margin-right: 10px;
-  height: 30px;
-  width: 150px;
+  padding: 20px 30px 20px 10px;
+  background-color: var(--bg-color);
+}
+.terminal-box_icon-group {
   overflow: hidden;
-  border-radius: 5px;
-  color: #333333;
-  background-color: blue;
+}
+.terminal-box_content-text {
+  padding: 0;
+  margin: 0;
+  color: var(--text-color);
+  font-weight: var(--text-weight);
+  font-size: var(--text-size);
+  line-height: 25px;
+  font-family: Arial, Helvetica, sans-serif;
+}
+.terminal-box_icon-wrapper {
+  position: relative;
+  display: flex;
+  align-items: center;
+  height: 30px;
+  margin: 0;
+  margin-right: 30px;
+  background: blue;
 }
 
-.heading-017:before {
+.terminal-box_icon-wrapper::before {
   position: absolute;
   top: -50%;
-  left: -50px;
-  z-index: 1;
+  left: -30px;
   transform: rotate(15deg);
-  width: 100px;
+  width: 80px;
   height: 200%;
   background-color: rgba(154, 154, 154, 1);
   content: '';
 }
 
-.heading-017 span {
-  margin: 0 1.1em;
-  color: #fff;
-  font-size: 0.8em;
+.terminal-box_icon-wrapper::after {
+  width: 0px;
+  height: 0px;
+  border: solid 15px blue;
+  border-color: transparent transparent transparent blue;
+  background-color: transparent;
+  margin: 0 auto 20px;
+  padding: 0;
+  position: absolute;
+  top: 0;
+  right: -30px;
+  content: '';
+}
+.terminal-box_icon-first {
+  position: relative;
+  margin: 0 1em;
+  color: black;
+}
+.terminal-box_icon-second-wrapper {
+  display: flex;
+  align-items: center;
+}
+.terminal-box_icon-second {
+  position: relative;
+  margin: 0 1em;
+  color: white;
 }
 
-.terminal-box_content p {
-  padding: 0;
-  margin: 0;
-  margin-left: 10px;
+/* .terminal-box_icon-path {
   color: white;
-  line-height: 25px;
   font-family: Arial, Helvetica, sans-serif;
-}
+} */
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import mdiVue from 'mdi-vue/v3'
+import * as mdiJs from '@mdi/js'
 
 import createUiComponent from './framework'
-createApp(App).use(createUiComponent).mount('#app')
+createApp(App).use(mdiVue, { icons: mdiJs }).use(createUiComponent).mount('#app')
 
 // createApp(App).mount('#app')
